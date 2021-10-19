7d2bd909d2604bf7a78f58d3e6557a9add8e1ec6
Laken Harville <rlakenharville@gmail.com>
2021-10-18 23:26:29 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine("remainder = " + remainder);
+            Console.WriteLine("quotient = " + quotient);
+            
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+            else
+            {
+                Console.WriteLine("Use the correct variables.");
+            }
+
+
+
+
+        }
+     
+
+    }
+}
